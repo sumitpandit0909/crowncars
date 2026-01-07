49cdd3cced8a1258a909699e08c7295067d78316
sumit pandit <its.sumitpandit@gmail.com>
2026-01-07 18:18:30 +0530

    output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   adapter: vercel(),
   image: {
       remotePatterns: [{ protocol: "https" }],
